d19d2ba3fb4446c7a6ae43dd2ca07544267405bf
Nad Inee <71218761+Nad-rb@users.noreply.github.com>
2021-01-30 12:22:21 +0800

    Update HelpCommand.ts

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -32,7 +32,7 @@ export class HelpCommand extends BaseCommand {
                     .setTitle(`${this.client.user?.username as string} - command list`)
                     .setColor(this.client.config.embedColor)
                     .setThumbnail(message.client.user?.displayAvatarURL() as string)
-                    .setFooter(`Use ${message.client.config.prefix}help <command> to get more information for command!`, "https://raw.githubusercontent.com/zhycorp/disc-11/stable/.github/images/info.png")
+                    .setFooter(`Use ${message.client.config.prefix}help <command> to get more information for command!`, "https://cdn.discordapp.com/attachments/795095199750946866/804929235990609970/dorami.gif")
             ).catch(e => this.client.logger.error("HELP_CMD_ERR:", e));
         }
     }
